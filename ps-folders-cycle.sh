b1b5e1992d7295cb97568d202497a2ac6db8bfd5
fluoroom <nico.vial.giudici@gmail.com>
2021-06-16 22:36:02 -0300

    bug fixes and minor changes

--- ps-folders-cycle.sh
+++ ps-folders-cycle.sh
@@ -15,11 +15,10 @@ APPS_DIR=/usr/share/applications
 CONFIG=$USER_HOME/.ps-folders-cycle
 if [[ -f "$CONFIG" ]]; then
     . $CONFIG
-    echo "
-Loaded config file ($CONFIG):"
+    echo = "Loaded config file ($CONFIG)";
 else 
-    echo "
-Config file ($CONFIG) not found. Using default configuration:"
+    notify-send "Config file ($CONFIG) not found. Using default configuration.";
+    echo "Config file ($CONFIG) not found. Using default configuration.";
 fi
 
 #set variables from defaults or config
@@ -30,21 +29,20 @@ echo "Colors (in order): ${colors[@]}
 Theme: $theme
 Interval: $interval seconds
 Shuffle: $shuffle
-Shuffle start: $startupNotif
+Shuffle start: $shuffleStart
 Startup notification: $startupNotif
 Color change notification: $colorNotif
 "
 
 if [ $interval -lt 10 ]; then
-    echo "You set an interval of $interval seconds? No way that's stable! We'll use 10 seconds. Ideal is 60 seconds or higher.
-    "
+    notify-send "You set an interval of $interval seconds? No way that's stable! We'll use 10 seconds. Ideal is 60 seconds or higher."
     interval=10
 fi
 colorsLength=${#colors[@]}
 actualIndex=0
 
 function notifyStartup () {
-    notify-send "papirus-folders-cycle started!";
+    notify-send "Papirus Folders Cycle started!";
 }
 function notifyColor () {
     notify-send "Folder color changed to ${1}";
@@ -70,7 +68,7 @@ while :; do   {
     fi 
     papirus-folders -t $theme -C ${colors[actualIndex]}
     if $colorNotif; then 
-    notifyColor ${colors[actualIndex]}
+    notifyColor ${colors[actualIndex]};
     fi
     if [ $shuffle == false ];  then
         actualIndex=$((actualIndex+1))
